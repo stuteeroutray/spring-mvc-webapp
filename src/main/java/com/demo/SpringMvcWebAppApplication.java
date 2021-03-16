2c6551f00c0a3f2aa8d6665d7bf415c9cb6374a5
Stutee Routray <stuteer@gmail.com>
2021-03-15 17:19:29 -0700

    using controller to run index.html

--- src/main/java/com/demo/SpringMvcWebAppApplication.java
+++ src/main/java/com/demo/SpringMvcWebAppApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringmvcApplication {
+public class SpringMvcWebAppApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringmvcApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcWebAppApplication.class, args);
+	}
+
 }
