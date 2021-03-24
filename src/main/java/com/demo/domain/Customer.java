6dbf00d271d3724628855868134e812b65de6af8
Stutee Routray <stuteer@gmail.com>
2021-03-24 16:55:37 -0700

    Implemented Embedded relationship using Address entity

--- src/main/java/com/demo/domain/Customer.java
+++ src/main/java/com/demo/domain/Customer.java
@@ -8,19 +8,16 @@ public class Customer implements DomainObject {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-
     @Version
     private Integer version;
-
     private String firstName;
     private String lastName;
     private String email;
     private String phoneNumber;
-    private String addressLine1;
-    private String addressLine2;
-    private String city;
-    private String state;
-    private String zipCode;
+    @Embedded
+    private Address billingAddress;
+    @Embedded
+    private Address shippingAddress;
 
     @OneToOne(cascade = CascadeType.PERSIST)
     private User user;
@@ -75,45 +72,21 @@ public class Customer implements DomainObject {
         this.phoneNumber = phoneNumber;
     }
 
-    public String getAddressLine1() {
-        return addressLine1;
-    }
+    public Address getBillingAddress() {
+		return billingAddress;
+	}
 
-    public void setAddressLine1(String addressLine1) {
-        this.addressLine1 = addressLine1;
-    }
+	public void setBillingAddress(Address billingAddress) {
+		this.billingAddress = billingAddress;
+	}
 
-    public String getAddressLine2() {
-        return addressLine2;
-    }
+	public Address getShippingAddress() {
+		return shippingAddress;
+	}
 
-    public void setAddressLine2(String addressLine2) {
-        this.addressLine2 = addressLine2;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
+	public void setShippingAddress(Address shippingAddress) {
+		this.shippingAddress = shippingAddress;
+	}
 
     public User getUser() {
         return user;
